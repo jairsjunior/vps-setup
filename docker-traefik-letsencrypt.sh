60be707ec8d83403f6d8b4abbf5716db1b2f0895
Jair de Souza Junior <detento112@gmail.com>
2020-07-07 19:59:15 -0300

    Correction at script

--- docker-traefik-letsencrypt.sh
+++ docker-traefik-letsencrypt.sh
@@ -13,7 +13,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && 
 apt-key fingerprint 0EBFCD88 && \
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
 apt-get update && \
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
 echo "Configure Swarm..."
 docker swarm init && \
